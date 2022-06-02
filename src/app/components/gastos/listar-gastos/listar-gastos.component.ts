0da353b87b898fdeaff408897b68099d870700c0
Yoan Estrada <yoanestrada01@gmail.com>
2022-06-01 21:16:09 -0500

    ADD: se termino el component ingresar gastos y se termino la logica del component listar gastos

--- src/app/components/gastos/listar-gastos/listar-gastos.component.ts
+++ src/app/components/gastos/listar-gastos/listar-gastos.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
   selector: 'app-listar-gastos',
   templateUrl: './listar-gastos.component.html',
   styleUrls: ['./listar-gastos.component.css']
 })
-export class ListarGastosComponent implements OnInit {
+export class ListarGastosComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription:Subscription;
+  presupuesto: number
+  restante:number
+  listGastos: any[]= []
+
+  constructor( private presupuestoService: PresupuestoService) {
+    this.presupuesto = 0
+    this.restante = 0
+    this.subscription =  this.presupuestoService.getGastos().subscribe(data => {
+      this.restante -= data.cantidad
+      this.listGastos.push(data)
+    })
+  }
 
   ngOnInit(): void {
+    this.presupuesto = this.presupuestoService.presupuesto
+    this.restante = this.presupuestoService.restante
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe()
   }
 
 }
